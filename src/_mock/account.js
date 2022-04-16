4dbbd3b8e0dffbeb9ab33f3eedf2176d6737c0ef
ssagar2411 <sagarsitaula8@gmail.com>
2022-04-16 18:10:19 +0545

    Initialization of project from mui template

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Sagar Sitaula',
+  email: 'info@gameland.com',
+  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+};
+
+export default account;
